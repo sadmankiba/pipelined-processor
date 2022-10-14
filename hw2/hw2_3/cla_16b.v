f0463c9c40afc395a107e0db01f95438413b3a60
sadmankiba <sadmankiba@roman.cs.wisc.edu>
2022-10-14 10:07:56 -0500

    Complete ALU with my CLA!

--- hw2/hw2_3/cla_16b.v
+++ hw2/hw2_3/cla_16b.v
@@ -4,22 +4,29 @@
     
     a 16-bit CLA module
 */
-module cla_16b(sum, c_out, ofl, sign, a, b, c_in);
+module cla_16b(sum, ofl, sign, a, b, c_in);
 
     // declare constant for size of inputs, outputs (N)
     parameter   N = 16;
 
     output [N-1:0] sum;
-    output         c_out, ofl;
+    output         ofl;
     input [N-1: 0] a, b;
     input          c_in;
     input          sign;
+    wire  [N:0]   c;
+    assign c[0] = c_in;
 
     // YOUR CODE HERE
     wire c4, c8, c12;
-    cla_4b CA0(sum[3:0], c4, a[3:0], b[3:0], c_in);
-    cla_4b CA1(sum[7:4], c8, a[7:4], b[7:4], c4);
-    cla_4b CA2(sum[11:8], c12, a[11:8], b[11:8], c8);
-    cla_4b CA3(sum[15:12], c_out, a[15:12], b[15:12], c12);
+    cla_4b CA0(sum[3:0], c[4:1], a[3:0], b[3:0], c_in);
+    cla_4b CA1(sum[7:4], c[8:5], a[7:4], b[7:4], c[4]);
+    cla_4b CA2(sum[11:8], c[12:9], a[11:8], b[11:8], c[8]);
+    cla_4b CA3(sum[15:12], c[16:13], a[15:12], b[15:12], c[12]);
+
+    // Overflow
+    wire cx;
+    xor2 XR(cx, c[15], c[16]);
+    mux2_1 MX(c[16], cx, sign, ofl);
 
 endmodule
--- hw2/hw2_3/cla_4b.v
+++ hw2/hw2_3/cla_4b.v
@@ -10,7 +10,7 @@ module cla_4b(sum, c_out, a, b, c_in);
     parameter   N = 4;
 
     output [N-1:0] sum;
-    output         c_out;
+    output [N-1:0] c_out;
     input [N-1: 0] a, b;
     input          c_in;
 
@@ -26,20 +26,20 @@ module cla_4b(sum, c_out, a, b, c_in);
     // C1 = G0 + P0C0
     wire pdc;
     and2 AD0(pdc, p0, c_in);
-    or2 OR0(c1, g0, pdc);
+    or2 OR0(c_out[0], g0, pdc);
 
     // C2 = G1 + P1G0 + P1P0C0
-    wire pdg0, pdg1, c2; 
+    wire pdg0, pdg1; 
     and2 AD1(pdg0, p1, g0);
     and3 AD2(pdg1, p1, p0, c_in);
-    or3 OR1(c2, g1, pdg0, pdg1);
+    or3 OR1(c_out[1], g1, pdg0, pdg1);
 
     // C3 = G2 + P2C2 = G2 + P2G1 + P2P1G0 + P2P1P0C0
-    wire pdg2, pdg3, pdg4, c3;
+    wire pdg2, pdg3, pdg4;
     and2 AD3(pdg2, p2, g1);
     and3 AD4(pdg3, p2, p1, g0);
     and4 AD5(pdg4, p2, p1, p0, c_in);
-    or4 OR2(c3, g2, pdg2, pdg3, pdg4);
+    or4 OR2(c_out[2], g2, pdg2, pdg3, pdg4);
 
     // C4 = G3 + P3*G2 + p3*p2*g1 + p3*p2*p1*g0 + p3*p2*p1*p0*c0
     wire pdg5, pdg6, pdg7, pdg8, pdg9, prg;
@@ -49,12 +49,12 @@ module cla_4b(sum, c_out, a, b, c_in);
     and4 AD9(pdg8, p2, p1, p0, c_in);
     and2 AD10(pdg9, p3, pdg8);
     or4 OR3(prg, pdg5, pdg6, pdg7, pdg9);
-    or2 OR4(c_out, g3, prg);
+    or2 OR4(c_out[3], g3, prg);
     
     xor2 XR0(sum[0], p0, c_in);
-    xor2 XR1(sum[1], p1, c1);
+    xor2 XR1(sum[1], p1, c_out[0]);
     // assign sum[3:2] = 2'b00;
-    xor2 XR2(sum[2], p2, c2);
-    xor2 XR3(sum[3], p3, c3);
+    xor2 XR2(sum[2], p2, c_out[1]);
+    xor2 XR3(sum[3], p3, c_out[2]);
 
 endmodule
