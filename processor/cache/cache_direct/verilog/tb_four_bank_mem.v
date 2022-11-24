3f18e53fcbe1cf8f8d69c00c2b0bf49b86c74ecc
sadmankiba <sadmankiba@cs.wisc.edu>
2022-11-23 19:30:36 -0600

    Print every cycle val

--- processor/cache/cache_direct/verilog/tb_four_bank_mem.v
+++ processor/cache/cache_direct/verilog/tb_four_bank_mem.v
@@ -8,7 +8,8 @@ module tb_four_bank_mem();
     reg [15:0]           DataIn;                 
     reg                  Rd;                     
     reg                  Wr;                     
-    reg                  createdump;             
+    reg                  createdump; 
+    reg [1:0]            bank;           
 
     wire                 clk;
     wire                 rst;
@@ -25,33 +26,31 @@ module tb_four_bank_mem();
                        .wr              (Wr),
                        .clk(clk), 
                        .rst(rst),
-                       .createdump      (1'b0)
+                       .createdump      (1'b0),
                        
                        /* output */
                        .data_out         (DataOut[15:0]),
                        .busy            (Busy),
                        .stall           (Stall),
-                       .err        (Err),
-                       );
+                       .err        (Err));
     
     wire [15:0]          DataOut_ref;
     wire                 Done_ref;
     wire                 Stall_ref;
     wire                 CacheHit_ref;
 
-    mem_system_ref ref(
-                      // Outputs
-                      .DataOut          (DataOut_ref[15:0]),
+    mem_system_ref ref(/* output */
+                      .DataOut          (DataOut_ref),
                       .Done             (Done_ref),
                       .Stall            (Stall_ref),
                       .CacheHit         (CacheHit_ref),
-                      // Inputs
+                      /* input */
                       .Addr             (Addr[15:0]),
                       .DataIn           (DataIn[15:0]),
                       .Rd               (Rd),
                       .Wr               (Wr),
-                      .clk( DUT.clkgen.clk),
-                      .rst( DUT.clkgen.rst) );
+                      .clk(clk),
+                      .rst(rst) );
    
     reg    reg_readorwrite;
     integer n_requests;
@@ -59,7 +58,7 @@ module tb_four_bank_mem();
     integer req_cycle;
     reg test_success;
    
-   
+    assign DataOut_ref = 16'd0; 
     initial begin
         Rd = 1'b0;
         Wr = 1'b0;
@@ -69,60 +68,63 @@ module tb_four_bank_mem();
         n_requests = 0;
         n_replies = 0;
         test_success = 1'b1;
+        req_cycle = 0;
     end
    
     always @ (posedge clk) begin
         #2; // simulation delay
-        n_replies = n_replies + 1;
-         
-        if (Rd) begin
-            $display("LOG: ReqNum %4d Cycle %8d ReqCycle %8d Rd Addr 0x%04x Value 0x%04x ValueRef 0x%04x\n",
-                n_replies,
-                DUT.clkgen.cycle_count,
-                req_cycle,
-                Addr,
-                DataOut,
-                DataOut_ref);
-            if (DataOut != DataOut_ref) begin
-                $display("ERROR");
-                test_success = 1'b0;
-            end
-        end
-        if (Wr) begin
-        $display("LOG: ReqNum %4d Cycle %8d ReqCycle %8d Wr Addr 0x%04x Value 0x%04x ValueRef 0x%04x\n",
-                    n_replies, DUT.clkgen.cycle_count, req_cycle, Addr, DataIn, DataIn);
-        end
-        Rd = 1'd0;
-        Wr = 1'd0;
-      
+        $display("");
 
+        if (!Stall) begin
+            n_replies = n_replies + 1;
+            // $display("DONE: ReqNum %0d Cycle %0d ", n_replies, clkgen.cycle_count, 
+            //     "ReqCycle %0d Rd %d Wr %d ", req_cycle, Rd, Wr, 
+            //     "Addr 0x%04x Bank %d DataOut 0x%04x ", Addr, bank, DataOut,
+            //     "DataOutRef 0x%04x DataIn 0x%04x ", DataOut_ref, DataIn);
+            // if (Rd) begin
+            //     if (DataOut != DataOut_ref) begin
+            //         $display("ERROR");
+            //         test_success = 1'b0;
+            //     end
+            // end
+            Rd = 1'd0;
+            Wr = 1'd0;
+        end
+        
         // change inputs for next cycle
       
-        #85;
-        if (!rst && (!Stall)) begin      
-            if (n_requests < 1000) begin
+        // #85;
+        if (!rst) begin      
+            if (n_requests < 20) begin
                 small_random_addr;
-            end else if (n_requests < 2000) begin
+            end else if (n_requests < 40) begin
                 full_random_addr;
             end else begin
                 end_simulation;
             end
-            if ( (Rd | Wr) && (!rst && (!Stall)) ) begin
-                req_cycle = DUT.clkgen.cycle_count;
+            if ( (Rd | Wr) && (!rst) ) begin
+                req_cycle = clkgen.cycle_count;
             end
         end
+
+        $display("EVERY: Cycle %0d Stall %d Busy %4b ", clkgen.cycle_count, Stall, Busy,
+                "Rd %d Wr %d ", Rd, Wr,
+                "Addr 0x%04x Bank %d DataOut 0x%04x ", Addr, Addr[2:1], DataOut);
+
+        $display("ReqCycle %0d Rd %d Wr %d ", req_cycle, Rd, Wr, 
+                "DataOutRef 0x%04x DataIn 0x%04x ", DataOut_ref, DataIn);
     end
 
     task check_dropped_request;
   	    begin	
             if (n_replies != n_requests) begin
                 if (Rd) begin
-                    $display("LOG: ReqNum %4d Cycle %8d ReqCycle %8d Rd Addr 0x%04x RefValue 0x%04x\n",
-                            n_replies, DUT.clkgen.cycle_count, req_cycle, Addr, DataOut_ref);
+                    $display("ERRLOG: ReqNum %4d Cycle %8d ReqCycle %8d Rd Addr 0x%04x RefValue 0x%04x\n",
+                            n_replies, clkgen.cycle_count, req_cycle, Addr, DataOut_ref);
                 end
                 if (Wr) begin
-                    $display("LOG: ReQNum %4d Cycle %8d ReqCycle %8d Wr Addr 0x%04x Value 0x%04x\n",
-                            n_replies, DUT.clkgen.cycle_count, req_cycle, Addr, DataIn);
+                    $display("ERRLOG: ReQNum %4d Cycle %8d ReqCycle %8d Wr Addr 0x%04x Value 0x%04x\n",
+                            n_replies, clkgen.cycle_count, req_cycle, Addr, DataIn);
                 end
                 $display("ERROR! Request dropped");
                 test_success = 1'b0;               
@@ -133,18 +135,21 @@ module tb_four_bank_mem();
 
     task full_random_addr;
         begin
-            if (!rst && (!Stall)  && (DUT.clkgen.cycle_count > 10)) begin
-                check_dropped_request;
-                reg_readorwrite = $random % 2;
+            if (!rst && (clkgen.cycle_count > 5)) begin
+                // check_dropped_request;
+                // reg_readorwrite = $random % 2;
+                reg_readorwrite = 1;
                 if (reg_readorwrite) begin
-                Wr = $random % 2;
-                Addr = ($random % 16'hffff) & 16'hFFFE;
-                DataIn = $random % 16'hffff;
-                Rd = ~Wr;
-                n_requests = n_requests + 1;               
+                    Wr = $random % 2;
+                    Addr = ($random % 16'hffff) & 16'hFFFE;
+                    DataIn = $random % 16'hffff;
+                    Rd = ~Wr;
+                    n_requests = n_requests + 1;
+                    $display("REQ: ReqCy: %0d Rd %d Wr %d Addr 0x%04x Bank %0d DataIn 0x%04x", 
+                        clkgen.cycle_count, Rd, Wr, Addr, Addr[2:1], DataIn);          
                 end else begin
-                Wr = 1'd0;
-                Rd = 1'd0;
+                    Wr = 1'd0;
+                    Rd = 1'd0;
                 end  // if (reg_readorwrite) 
             end // if (!Stall)
         end
@@ -154,18 +159,21 @@ module tb_four_bank_mem();
         // all addresses will fit in cache
         // should generate a lot of cache hits
         begin
-            if (!rst && (!Stall) && (DUT.clkgen.cycle_count > 10) ) begin
-                check_dropped_request;            
-                reg_readorwrite = $random % 2;
+            if (!rst && (clkgen.cycle_count > 5) ) begin
+                // check_dropped_request;            
+                // reg_readorwrite = $random % 2;
+                reg_readorwrite = 1;
                 if (reg_readorwrite) begin
-                Wr = $random % 2;
-                Addr = (($random % 16'hffff) & 16'h07FE) | 16'h6000;
-                DataIn = $random % 16'hffff;
-                Rd = ~Wr;
-                n_requests = n_requests + 1;               
+                    Wr = $random % 2;
+                    Addr = (($random % 16'hffff) & 16'h07FE) | 16'h6000;
+                    DataIn = $random % 16'hffff;
+                    Rd = ~Wr;
+                    n_requests = n_requests + 1;    
+                    $display("REQ: ReqCy: %0d Rd %d Wr %d Addr 0x%04x Bank %0d DataIn 0x%04x", 
+                        clkgen.cycle_count, Rd, Wr, Addr, Addr[2:1], DataIn);           
                 end else begin
-                Wr = 1'd0;
-                Rd = 1'd0;
+                    Wr = 1'd0;
+                    Rd = 1'd0;
                 end  // if (reg_readorwrite) 
             end // if (!Stall)
         end
@@ -176,7 +184,7 @@ module tb_four_bank_mem();
             $display("LOG: Done Requests: %10d Replies: %10d Cycles: %10d",
                     n_requests,
                     n_replies,
-                    DUT.clkgen.cycle_count);
+                    clkgen.cycle_count);
             if (!test_success)  begin
                 $display("Test status: FAIL");
             end else begin
