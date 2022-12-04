60a5821ae46532727794a5e9220ef509bd885d48
sadmankiba <sadmankiba@cs.wisc.edu>
2022-12-03 21:38:38 -0600

    Copy bug fixes to cache-assoc

--- processor/cache/cache_assoc/verilog/mem_system.v
+++ processor/cache/cache_assoc/verilog/mem_system.v
@@ -24,8 +24,8 @@ module mem_system(/*AUTOARG*/
     output err;
 
     /* cache controller */
-    wire [2:0] stateRegOut, stateRegIn;
-    wire [8:0] stateRegDOut;
+    wire [2:0] stateReg, stateRegIn;
+    wire [7:0] stateRegD;
     wire isWaitForReq, isRead, isReadC1, isAllocate, isWrite,
         isCompareTag;
     wire bankBusy, readBankNBusy, isRBNZero;
@@ -34,7 +34,7 @@ module mem_system(/*AUTOARG*/
     
     /* mem/cache */
     wire [15:0] cDataIn, c0DataOut, c1DataOut;
-    wire [1:0] cOffsetIn;
+    wire [2:0] cOffsetIn;
     wire c0WriteIn, c1WriteIn, cEnable, c0ValidIn, c1ValidIn;
     wire c0Hit, c1Hit, c0Dirty, c1Dirty, c0Valid, c1Valid, c0Err, c1Err, 
         c0HitAndValid, c1HitAndValid, cHitAndValid;
@@ -44,7 +44,9 @@ module mem_system(/*AUTOARG*/
 
     wire [15:0] memDataOut, memAddrIn;
     wire [1:0] bank, readBankNIn, readBankN;
-    wire memStall, memErr;
+    wire [3:0] rbnp1, rbnm1, carryOut;
+    wire memWriteIn, memReadIn, memStall, memErr,
+        readFetched, readFetchedIn;
     wire [3:0] memBusy;
 
     /*
@@ -52,16 +54,16 @@ module mem_system(/*AUTOARG*/
     0- WaitForRequest, 1- Read, 2- ReadCycle1, 3- Allocate,
     4- Write, 5- CompareTag
     */
-    dff STATEREG [2:0] (.q(stateRegOut), .d(stateRegIn), .clk(clk), .rst(rst));
+    dff STATEREG [2:0] (.q(stateReg), .d(stateRegIn), .clk(clk), .rst(rst));
     
     /* Get current state */
-    decoder3_8 DCDST(.in(stateRegOut), .out(stateRegDOut));
-    assign isWaitForReq = stateRegDOut[0];
-    assign isRead = stateRegDOut[1];
-    assign isReadC1 = stateRegDOut[2];
-    assign isAllocate = stateRegDOut[3];
-    assign isWrite = stateRegDOut[4];
-    assign isCompareTag = stateRegDOut[5];
+    decoder3_8 DCDST(.in(stateReg), .out(stateRegD));
+    assign isWaitForReq = stateRegD[0];
+    assign isRead = stateRegD[1];
+    assign isReadC1 = stateRegD[2];
+    assign isAllocate = stateRegD[3];
+    assign isWrite = stateRegD[4];
+    assign isCompareTag = stateRegD[5];
 
     /* Get mem output */
     assign bank = Addr[2:1];
@@ -69,7 +71,9 @@ module mem_system(/*AUTOARG*/
             .S(bank), .Out(bankBusy));
 
     /* Get/set mem system registers */
-    assign readBankNIn = isReadC1? (readBankNIn + 1) : readBankN;
+    cla_4b SBN (.sum(rbnp1), .c_out(carryOut), 
++        .a({2'b00, readBankN}), .b(4'b0001), .c_in(1'b0));
+    assign readBankNIn = isReadC1? rbnp1[1:0] : readBankN;
     dff RBANKN [1:0] (.q(readBankN), .d(readBankNIn), .clk(clk), .rst(rst));
     mux4_1 BNNBS (.InD(memBusy[3]),  .InC(memBusy[2]), .InB(memBusy[1]), .InA(memBusy[0]),   
             .S(readBankN), .Out(readBankNBusy));
@@ -78,19 +82,23 @@ module mem_system(/*AUTOARG*/
     assign c0HitAndValid = c0Hit & c0Valid;
     assign c1HitAndValid = c1Hit & c1Valid;
     assign cHitAndValid = c0HitAndValid | c1HitAndValid;
-    assign readDoneIn = isWaitForReq? 1'b0: (isCompareTag & cHitAndValid);
+    assign readDoneIn = isWaitForReq? 1'b0: (isCompareTag & cHitAndValid & Rd);
     dff RDDONE (.q(readDone), .d(readDoneIn), .clk(clk), .rst(rst));
 
     assign writeDoneIn = isWaitForReq? 1'b0: isWrite & (~bankBusy);
     dff WRDONE (.q(writeDone), .d(writeDoneIn), .clk(clk), .rst(rst));
 
+    assign readFetchedIn = (isAllocate & isRBNZero)? 1'b1: readFetched & (~isWaitForReq);
+    dff RDF (.q(readFetched), .d(readFetchedIn), .clk(clk), .rst(rst));
+
     assign victimwayIn = victimway ^ ((isCompareTag & Rd) | c0WriteIn | c1WriteIn);
     dff VC (.q(victimway), .d(victimwayIn), .clk(clk), .rst(rst));
     
     /* Set mem/cache input */
-    assign memAddrIn = (Wr)? Addr: {Addr[15:3], readBankN, 1'b0};
     assign cDataIn = (isCompareTag & Wr)? DataIn: memDataOut;
-    assign cOffsetIn = (isAllocate)? {readBankN, 1'b0} : Addr[2:0];
+    cla_4b ADN (.sum(rbnm1), .c_out(carryOut), 
+        .a({2'b00, readBankN}), .b(4'b1111), .c_in(1'b0));
+    assign cOffsetIn = (isAllocate)? {rbnm1[1:0], 1'b0} : Addr[2:0];
     assign cEnable = ~rst;
     
     mux4_1 WRC (.InD(victimway), .InC(1'b0), .InB(1'b1), .InA(1'b0), 
@@ -100,16 +108,21 @@ module mem_system(/*AUTOARG*/
     assign c0ValidIn = (isAllocate & (~writeCache));
     assign c1ValidIn = (isAllocate & writeCache);
     
+    assign memAddrIn = (Wr)? Addr: {Addr[15:3], readBankN, 1'b0};
+    assign memWriteIn = isWrite & Wr;
+    assign memReadIn = isRead & Rd;
+
     /* Set mem system output signals */
     mux2_1_16b DO (.InB(c1DataOut), .InA(c0DataOut), .S(c1HitAndValid), .Out(DataOut));
     assign Stall = isCompareTag | isRead | isReadC1 | isAllocate | isWrite;
     assign Done = readDone | writeDone;
-    assign CacheHit = cHitAndValid & Rd;
+    assign CacheHit = cHitAndValid & Rd & (~readFetched);
     assign err = c0Err | c1Err | memErr;
     
     /* Set next state */
-    assign waitReqIn = (isCompareTag & cHitAndValid & Rd) | (isWrite & (~bankBusy));
-    assign compareTagIn = (isWaitForReq & (~readDone) & (~writeDone)) 
+    assign waitReqIn = (isWaitForReq & (~(Rd | Wr))) | (isCompareTag & cHitAndValid & Rd) 
+            | (isWrite & (~bankBusy));
+    assign compareTagIn = (isWaitForReq & (Rd | Wr)) 
             | (isAllocate & isRBNZero);
     assign readIn = (isCompareTag & Rd & (~cHitAndValid)) | (isRead & readBankNBusy)
             | (isAllocate & (~isRBNZero));
@@ -174,8 +187,8 @@ module mem_system(/*AUTOARG*/
                         .createdump        (createdump),
                         .addr              (memAddrIn),
                         .data_in           (DataIn),
-                        .wr                (Wr),
-                        .rd                (Rd));
+                        .wr                (memWriteIn),
+                        .rd                (memReadIn));
    
    // your code here
 
